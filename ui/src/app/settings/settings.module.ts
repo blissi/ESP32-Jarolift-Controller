3891407aae322108f6b500f3fa3955650422cbdb
Steven Weiß <info@code-factory.de>
2025-03-22 19:43:40 +0100

    Added translation service. Copied the HTML for the settings page.

--- ui/src/app/settings/settings.module.ts
+++ ui/src/app/settings/settings.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { SettingsPageComponent } from './settings-page/settings-page.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 const routes: Routes = [
   {path: "", component: SettingsPageComponent }
 ];
 
 @NgModule({
-  declarations: [],
+  declarations: [SettingsPageComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    TranslateModule.forChild()
   ]
 })
 export class SettingsModule { }
