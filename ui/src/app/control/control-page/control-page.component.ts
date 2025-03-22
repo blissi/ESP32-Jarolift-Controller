6d5a9824d397a20635029337f367ce2627b9e9a4
Steven Weiß <info@code-factory.de>
2025-03-22 09:27:09 +0100

    - Added a control for controlling the channels. - Added CSS styles and icons.

--- ui/src/app/control/control-page/control-page.component.ts
+++ ui/src/app/control/control-page/control-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: false,
   selector: 'app-control-page',
-  imports: [],
   templateUrl: './control-page.component.html',
   styleUrl: './control-page.component.scss'
 })
 export class ControlPageComponent {
-
+  channels: number[] = [0, 1, 2, 3, 4];
 }
